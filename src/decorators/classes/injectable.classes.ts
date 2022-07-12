2fb98c3176ad4367758be5132e2e6827bf0a74f1
Dan Pham <s5k@github.io>
2022-07-12 22:16:59 +0700

    Remove unused files

--- src/decorators/classes/injectable.classes.ts
+++
@@ -1,7 +0,0 @@
-import { container } from "../../instances/container";
-
-export function Injectable(token: string, args: any[] = []): Function {
-	return function <T extends { new (...args: any[]): {} }>(target: T): void {
-		container.setProvider(token, new target(...args));
-	};
-}
--- src/routes/media.ts
+++
@@ -1,8 +0,0 @@
-import { Router as ExpressRouter } from "express";
-import MediaController from "../controllers/media.controller";
-const router = ExpressRouter();
-const instanceMediaController = MediaController();
-
-router.get("/", instanceMediaController.index);
-
-export default router;
